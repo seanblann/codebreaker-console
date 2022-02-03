aa49e969b9db4b091e4892441b24360dc06f2032
seanblann <sblann@cnm.edu>
2022-02-03 14:58:30 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+//TODO  Read command-line arguments for game difficulty.
+//TODO  Request (POST) start of new game from service.
+//TODO  While the game is not completed:
+//            - Get user's guess.
+//            - Send guess (POST) to service.
+//            - Display matches (from server response) to user.
+//TODO:  Display statistics (# of guesses, time to solve) to user.
+  }
+
+}
