bef28f45f1411f680ee25469a2ed8bf0c3b88142
seanblann <sblann@cnm.edu>
2022-02-03 16:59:43 -0700

    Starting console-mode implementation.

---
+++ src/main/java/edu/cnm/deepdive/model/Game.java
@@ -0,0 +1,77 @@
+package edu.cnm.deepdive.model;
+
+import com.google.gson.annotations.Expose;
+import java.util.Date;
+
+public class Game {
+
+
+  @Expose
+  private String id;
+
+  @Expose
+  private Date created;
+
+  @Expose
+  private String pool;
+
+  @Expose
+  private int length;
+
+  @Expose
+  private boolean solved;
+
+  @Expose
+  private String text;
+
+
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(Date created) {
+    this.created = created;
+  }
+
+  public String getPool() {
+    return pool;
+  }
+
+  public void setPool(String pool) {
+    this.pool = pool;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public boolean isSolved() {
+    return solved;
+  }
+
+  public void setSolved(boolean solved) {
+    this.solved = solved;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+}
---
+++ src/main/java/edu/cnm/deepdive/service/CodebreakerServiceProxy.java
@@ -0,0 +1,55 @@
+package edu.cnm.deepdive.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.model.Game;
+import java.io.IOException;
+import java.util.Properties;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+
+public interface CodebreakerServiceProxy {
+
+  String ISO_8601_DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
+  @POST("games/")
+
+  Call<Game> startGame(@Body Game game);
+
+  static CodebreakerServiceProxy getInstance(){
+    return InstanceHolder.INSTANCE;
+  }
+  class InstanceHolder {
+
+    private static final String PROPERTIES_FILE = "local.properties";
+    private static final String BASE_URL_KEY = "base_url";
+    private static final CodebreakerServiceProxy INSTANCE;
+
+
+
+    static {
+      try {
+        Gson gson = new GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .setDateFormat(ISO_8601_DATETIME_FORMAT)
+            .create();
+        Properties props = new Properties();
+        props.load(InstanceHolder.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE));
+        Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(props.getProperty(BASE_URL_KEY))
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build();
+        INSTANCE = retrofit.create(CodebreakerServiceProxy.class);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+
+  }
+
+}
