fb5debae4cf134e358c1f79edd5fe01a322af1aa
seanblann <sblann@cnm.edu>
2022-02-07 10:26:39 -0700

    Refactored for internationalization.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive;
 
 import edu.cnm.deepdive.controller.Player;
 import java.io.IOException;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     //TODO  Read command-line arguments for game difficulty.
-    Player player = new Player(System.in, System.out);
+    Scanner scanner = new Scanner(System.in);
+    ResourceBundle bundle = ResourceBundle.getBundle("strings");
+    Player player = new Player(scanner, System.out, bundle);
     player.play("ABCDEF", 3);
   }
 
--- src/main/java/edu/cnm/deepdive/controller/Player.java
+++ src/main/java/edu/cnm/deepdive/controller/Player.java
@@ -3,49 +3,54 @@ package edu.cnm.deepdive.controller;
 import edu.cnm.deepdive.model.Game;
 import edu.cnm.deepdive.model.Guess;
 import edu.cnm.deepdive.service.GameRepository;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.Reader;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Player {
 
-  private final GameRepository repository;
-  private final InputStream input;
-  private final PrintStream output;
+  private static final String GUESS_PROMPT_FORMAT_KEY = "guess_prompt_format";
+  private static final String GUESS_OUTCOME_FORMAT_KEY = "guess_outcome_format";
+  private static final String SUMMARY_FORMAT_KEY = "summary_format";
 
-  public Player(InputStream input, PrintStream output) {
-    this.input = input;
+  private final GameRepository repository;
+  private final Scanner scanner;
+  private final PrintStream output;
+  private final ResourceBundle bundle;
+
+  public Player(Scanner scanner, PrintStream output,
+      ResourceBundle bundle) {
+    this.scanner = scanner;
     this.output = output;
+    this.bundle = bundle;
     repository = new GameRepository();
   }
 
   public void play(String pool, int length) throws IOException {
-    try (
-        Reader reader = new InputStreamReader(input);
-        BufferedReader buffer = new BufferedReader(reader);
-    ) {
-      Game game = repository.startGame(pool, length);
-      do {
-        String input = getGuess(game, buffer);
-        Guess guess = repository.submitGuess(game, input);
-        displayOutcome(guess);
-      } while (!game.isSolved());
-      //TODO: Display ending summary.
-    }
+    Game game = repository.startGame(pool, length);
+    Guess guess;
+    do {
+      String input = getGuess(game);
+      guess = repository.submitGuess(game, input);
+      displayOutcome(guess);
+    } while (!game.isSolved());
+    long endTime = guess.getCreated().getTime();
+    //TODO: Explore starting time from first guess.
+    long elapsedTime = endTime - game.getCreated().getTime();
+    long seconds = Math.round(elapsedTime / 1000.0);
+    System.out.printf(bundle.getString(SUMMARY_FORMAT_KEY), game.getText(), game.getGuesses().size(), seconds);
   }
 
-  private String getGuess(Game game, BufferedReader buffer) throws IOException {
-    output.printf("Pool: \"%s\"; length: %d.%nEnter guess: ", game.getPool(), game.getLength());
-    String input = buffer.readLine().trim();
+  private String getGuess(Game game) {
+    output.printf(bundle.getString(GUESS_PROMPT_FORMAT_KEY), game.getPool(), game.getLength());
+    String input = scanner.nextLine().trim();
     //TODO: Validate input using the pool and length.
     return input;
   }
 
   private void displayOutcome(Guess guess) {
-    output.printf("Exact matches: %d; near matches: %d.%n",
+    output.printf(bundle.getString(GUESS_OUTCOME_FORMAT_KEY),
         guess.getExactMatches(), guess.getNearMatches());
     //TODO: Include appropriate indicator if solved.
   }
